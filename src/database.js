46b9fb6461f58ed0f21eb9175dbf28bf997f64e7
Jorge <jhessle04@gmail.com>
2022-06-24 15:07:40 -0300

    N

--- src/database.js
+++ src/database.js
@@ -6,7 +6,6 @@ const dotenv = require('dotenv'); dotenv.config();
       useNewUrlParser: true,
       useUnifiedTopology: true
 
-      
   }) 
       .then(db => console.log(`DB is connected`))
       .catch(err => console.error(err));
