7e58ec4fffc9091e29cae1422cfa0635e9f23699
Jorge <jhessle04@gmail.com>
2022-12-05 20:39:47 -0300

    N

--- src/index.js
+++ src/index.js
@@ -22,9 +22,6 @@ require('./config/passport');
 const middleware=require('./config/middleware');
 
  
-
-
- 
 //settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
